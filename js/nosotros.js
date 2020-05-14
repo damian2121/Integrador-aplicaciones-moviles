4231f68107fb88ac7b106ac05bb7634fa9b10c23
lmonzongithub <57239949+lmonzongithub@users.noreply.github.com>
2020-05-14 10:37:24 -0300

    agrego nosotros.html, css y js con el mismo nombre que el html

---
+++ js/nosotros.js
@@ -0,0 +1,21 @@
+function initMap(){
+    const ubicacion= new Localizacion(()=>{
+        const mylatlng={
+            lat: ubicacion.latitude,
+            lng: ubicacion.longitude
+        };
+        var texto= '<h1> Aquí estamos</h1>'
+        const options= {
+            center: mylatlng,
+            zoom: 15
+        }
+        var map= document.getElementById('map');
+        const mapa= new google.maps.Map(map,options);
+        const marcador= new google.maps.Marker({
+            position: mylatlng,
+            map: mapa,
+            title: "aqui estamos"
+
+        });
+    });
+}
